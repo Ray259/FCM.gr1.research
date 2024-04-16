5c38361d929cce9dffee9fa703498d482514441a
Ray <elninothepea@gmail.com>
2024-04-16 21:51:19 +0700

    gui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import streamlit as st
 
 class FCM:
     def __init__(self, data, clusters=2, m=2, eps=0.01, lmax=50):
@@ -63,22 +64,52 @@ class FCM:
 # u = np.array([[0.8, 0.7, 0.2, 0.1], 
 #               [0.1, 0.1, 0.7, 0.8],
 #               [0.1, 0.2, 0.1, 0.1],])
-Y = np.random.randint(100, size=(10, 2))
-print("Data: ", Y)
-u = np.random.rand(2, 10)
-u = u / np.sum(u, axis=0, keepdims=True)
-m = 2
+# Y = np.random.randint(100, size=(10, 2))
+# print("Data: ", Y)
+# u = np.random.rand(2, 10)
+# u = u / np.sum(u, axis=0, keepdims=True)
+# m = 2
 
-o = FCM(data=Y)
-o.u = u
+# o = FCM(data=Y)
+# o.u = u
 
-centers = o.cal_centers()
-print("Centers:")
-print(centers)
+# centers = o.cal_centers()
+# print("Centers:")
+# print(centers)
 
-updated_u = o.update_membership(centers)
-print("Updated membership:")
-print(updated_u)
+# updated_u = o.update_membership(centers)
+# print("Updated membership:")
+# print(updated_u)
 
-print("=====================================")
-final = o.loop()
+# print("=====================================")
+# final = o.loop()
+
+def main():
+    st.title('FCM Algorithm Demo')
+
+    st.sidebar.header('Parameters')
+    clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10, value=2)
+    m = st.sidebar.slider('Fuzziness Coefficient (m)', min_value=1.1, max_value=5.0, value=2.0)
+    eps = st.sidebar.slider('Epsilon', min_value=0.001, max_value=0.1, value=0.01)
+    lmax = st.sidebar.slider('Maximum number of iterations', min_value=10, max_value=100, value=50)
+
+    data_generated = st.checkbox('Generate Random Data')
+    if data_generated:
+        n_samples = st.number_input('Number of samples', value=100)
+        Y = np.random.randint(100, size=(n_samples, 2))
+        st.write('Generated Data:')
+        st.write(Y)
+    else:
+        st.write('Please upload your data.')
+
+    o = FCM(data=Y, clusters=clusters, m=m, eps=eps, lmax=lmax)
+    st.write('Initial Membership:')
+    st.write(o.u)
+
+    if st.button('Run Algorithm'):
+        updated_u = o.loop()
+        st.write('Final Membership:')
+        st.write(updated_u)
+
+if __name__ == '__main__':
+    main()
